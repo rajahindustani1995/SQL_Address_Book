3a18375877b3e6a7676778dbfb6c8ed9f23f43ff
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-07-27 17:56:26 +0530

    [Rajesh] Added : Insert Contacts into AddressBook Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -15,3 +15,9 @@ Phone bigint not null,
 Email varchar(50) not null);
 
 select * from AddressBook;
+
+-------UC3 Insert Contacts into AddressBook table------
+insert into AddressBook values
+('Ashish', 'Sutar', 'Katraj', 'Pune', 'Maharashtra', 411058, 9545443210, 'ashish.sutar@gmail.com'),
+('Sam', 'Lodha', 'Kalleshwara', 'Allipura', 'Karnataka', 583219, 8362331741, 'sam.lodha@gmail.com'),
+('Pooja', 'Patil', 'Nerul', 'Navi Mumbai', 'Maharashtra', 400041, 8342587361, 'pooja.patil@gmail.com');
