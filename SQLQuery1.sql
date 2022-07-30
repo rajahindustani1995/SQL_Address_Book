2c3136ab28c06ad1d0ae426f66e9a12716d92a24
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-07-31 01:33:37 +0530

    [Rajesh] Added : Draw Entity Relationship Diagram

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -71,3 +71,65 @@ values('Pramod', 'Ambavale', 'Chembur', 'Mumbai', 'Maharashtra', 400034, 9865443
 ('Pramod', 'Ambavale', 'Chembur', 'Mumbai', 'Maharashtra', 400034, 9865443486, 'pramod.ambavale@gmail.com','Family');
 
 select * from AddressBook;
+
+-------UC12 - Creating table using Normalization and ER Diagram --
+Create table Address_Book1(
+AddressBookId Int Identity(1,1) Primary Key,
+AddressBookName varchar(100));
+
+Create table PersonDetail1(   
+PersonId Int Identity(1,1) Primary Key,
+AddressBookId Int Foreign Key References Address_Book1(AddressBookId),
+FirstName varchar(50),
+LastName varchar(50),
+Address varchar(100),
+City varchar(50),
+State varchar(50),
+Zip int,
+PhoneNumber bigint,
+Email_ID varchar(50) );
+
+CREATE table TypePerson(
+PersonTypeId Int Identity(1,1) Primary Key,
+PersonType varchar(50) );
+
+CREATE table PersonsDetail_Type1(
+PersonId Int Foreign Key References PersonDetail1(PersonId),
+PersonTypeId Int Foreign Key References TypePerson(PersonTypeId)  );
+
+CREATE table Employee_Department1(
+PersonId Int Foreign Key References PersonDetail1(PersonId),
+EmployeeID Int  ,
+DepartmentID int );
+				
+select *from Address_Book1;
+select *from PersonDetail1;
+select *from TypePerson;
+select *from PersonsDetail_Type1;
+select *from Employee_Department1;
+
+INSERT INTO Address_Book1(AddressBookName) Values('Home'),('school'),('college'),(' office');  
+select *from Address_Book1;
+
+
+
+--------Inserting values into persontype--
+INSERT INTO TypePerson(PersonType) VALUES('Family'),('schoolFriend'),('Friend'),('Colleague');
+select *from TypePerson;
+
+--------Insert values in person detail table--
+Insert INTO PersonDetail1 VALUES
+(1,'Ashish', 'Sutar', 'Katraj', 'Pune', 'Maharashtra', 411058, 9545443210, 'ashish.sutar@gmail.com'),
+(2,'Sam', 'Lodha', 'Kalleshwara', 'Allipura', 'Karnataka', 583219, 8362331741, 'sam.lodha@gmail.com'),
+(3,'Pooja', 'Patil', 'Nerul', 'Navi Mumbai', 'Maharashtra', 400041, 8342587361, 'pooja.patil@gmail.com'),
+(4,'Pramod', 'Ambavale', 'Chembur', 'Mumbai', 'Maharashtra', 400034, 9865443486, 'pramod.ambavale@gmail.com');
+
+select *from PersonDetail1;
+
+---------Insert values in person detail type--
+INSERT INTO PersonsDetail_Type1(PersonId,PersonTypeId) VALUES(1,1),(2,3),(3,4),(4,2);
+select *from PersonsDetail_Type1;
+
+--------Insert values in Employee_Department1 table--
+INSERT INTO Employee_Department1 VALUES(1,123,818),(2,456,19112),(3,789,4512),(4,244,161815)
+select *from Employee_Department1;
